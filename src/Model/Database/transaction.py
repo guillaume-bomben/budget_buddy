cdddeeb96c08c1762666229d4c9681876ecaa489
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-03-27 13:49:23 +0100

    Class for CRUD in table transaction

---
+++ src/Model/Database/transaction.py
@@ -0,0 +1,23 @@
+from DB import DB
+
+class User(DB):
+    def __init__(self):
+        super().__init__()
+        self.data_list = []
+        self.read()
+        
+    def create(self,lastname,firstname,email,password):
+        query = "INSERT INTO user (last_name,first_name,email,password) VALUES (%s,%s,%s,%s)"
+        param = (lastname,firstname,email,password)
+        self.executeQuery(query,param)
+    
+    def read(self):
+        query = "SELECT * FROM user"
+        self.data_list = []
+        for line in self.fetch(query):
+            self.data_list.append(line)
+    
+    def delete(self,id):
+        query = "DELETE FROM user WHERE id = %s"
+        param = (id,)
+        self.executeQuery(query,param)
