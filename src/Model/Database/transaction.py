b4c656a61de8cf7d9cad10dfab56088af5e358e2
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-03-28 10:19:35 +0100

    add geter on transaction table

--- src/Model/Database/transaction.py
+++ src/Model/Database/transaction.py
@@ -1,23 +1,43 @@
 from DB import DB
 
-class User(DB):
+class transaction(DB):
     def __init__(self):
         super().__init__()
         self.data_list = []
         self.read()
-        
-    def create(self,lastname,firstname,email,password):
-        query = "INSERT INTO user (last_name,first_name,email,password) VALUES (%s,%s,%s,%s)"
-        param = (lastname,firstname,email,password)
+
+    def create(self,id_user_send,id_user_recive,description,amount,type):
+        query = "INSERT INTO transaction (id_user_send,id_user_recive,description,amount,type) VALUES (%s,%s,%s,%s,%s)"
+        param = (id_user_send,id_user_recive,description,amount,type)
         self.executeQuery(query,param)
-    
+
     def read(self):
-        query = "SELECT * FROM user"
+        query = "SELECT * FROM transaction"
         self.data_list = []
         for line in self.fetch(query):
             self.data_list.append(line)
-    
+
     def delete(self,id):
-        query = "DELETE FROM user WHERE id = %s"
+        query = "DELETE FROM transaction WHERE id = %s"
         param = (id,)
         self.executeQuery(query,param)
+
+    def get_transaction(self,id):
+        query = "SELECT * FROM transaction WHERE id = %s"
+        param = (id,)
+        return self.fetch(query,param)
+
+    def get_id(self,id_user_send,id_user_recive,description,amount,type):
+        query = "SELECT id FROM transaction WHERE id_user_send = %s AND id_user_recive = %s AND description = %s AND amount = %s AND type = %s"
+        param = (id_user_send,id_user_recive,description,amount,type)
+        return self.fetch(query,param)[0]['id']
+
+    def get_id_user_send(self,id):
+        query = "SELECT id_user_send FROM transaction WHERE id = %s"
+        param = (id,)
+        return self.fetch(query,param)
+
+    def get_id_user_recive(self,id):
+        query = "SELECT id_user_recive FROM transaction WHERE id = %s"
+        param = (id,)
+        return self.fetch(query,param)
