b49fb687c7cc292148063742ecf28e932e88e713
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-03-28 09:47:31 +0100

    display home page

--- src/view/Windows.py
+++ src/view/Windows.py
@@ -1,4 +1,5 @@
 from src.view.LoginScreen import LoginScreen
+from src.view.home import Home
 import tkinter as tk
 import threading
 import time
@@ -27,6 +28,26 @@ class Windows:
                 if login_screen.statut == "login":
                     self.information = login_screen.information
                     print(self.information)
+                    break
+                else:
+                    print("waiting")
+                time.sleep(1)
+        threading.Thread(target=check).start()
+
+    def home_page(self):
+        home = Home(self.master)
+        self.statut = "home"
+        
+        def check():
+            while self.thread_flag:
+                if home.statut == "logout":
+                    self.statut = "logout"
+                    break
+                elif home.statut == "send":
+                    self.statut = "send"
+                    break
+                elif home.statut == "recive":
+                    self.statut = "recive"
                     break
                 else:
                     print("waiting")
