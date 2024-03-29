51202e48175892d00d977b1fadf716978bec75e5
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-03-29 08:44:27 +0100

    add frame and change position display for widget in LoginScreen.py

--- src/view/LoginScreen.py
+++ src/view/LoginScreen.py
@@ -1,33 +1,34 @@
 import tkinter as tk
 from tkinter import messagebox
 
-class LoginScreen:
+class LoginScreen(tk.Frame):
     def __init__(self,master):
-        self.master = master
-        self.master.title("Login")
-        self.master.geometry("300x200")
+        super().__init__(master)
         
         self.statut = None
         self.information = []
         self.widgets()
     
     def widgets(self):
-        self.label_email = tk.Label(self.master,text="email")
+        frame = tk.Frame(master=self)
+        frame.pack()
+        
+        self.label_email = tk.Label(master=frame,text="email")
         self.label_email.pack()
         
-        self.entry_email = tk.Entry(self.master)
+        self.entry_email = tk.Entry(master=frame)
         self.entry_email.pack()
         
-        self.label_password = tk.Label(self.master,text="password")
+        self.label_password = tk.Label(master=frame,text="password")
         self.label_password.pack()
         
-        self.entry_password = tk.Entry(self.master,show="*")
+        self.entry_password = tk.Entry(master=frame,show="*")
         self.entry_password.pack()
         
-        self.button_login = tk.Button(self.master,text="Login",command=self.login)
+        self.button_login = tk.Button(master=frame,text="Login",command=self.login)
         self.button_login.pack()
         
-        self.register = tk.Button(self.master,text="Register",command=self.register)
+        self.register = tk.Button(master=frame,text="Register",command=self.register)
         self.register.pack()
     
     def login(self):
