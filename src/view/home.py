51b60016526fcd976c6e86dba4cbfaca3523f889
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-03-29 08:45:04 +0100

    add frame and change position display for widget in home.py

--- src/view/home.py
+++ src/view/home.py
@@ -3,24 +3,25 @@ import tkinter as tk
 class Home(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.master.title("Home")
-        self.master.geometry("400x400")
 
         self.statut = None
         self.widgets()
         self.master.mainloop()
         
     def widgets(self):
-        self.balance_label = tk.Label(self.master, text="solde : 0")
+        frame = tk.Frame(master=self)
+        frame.pack()
+        
+        self.balance_label = tk.Label(master=frame, text="solde : 0")
         self.balance_label.pack()
         
-        self.button_logout = tk.Button(self.master, text="déconnexion", command=self.logout)
+        self.button_logout = tk.Button(master=frame, text="déconnexion", command=self.logout)
         self.button_logout.pack()
         
-        self.button_send = tk.Button(self.master, text="depense", command=self.send)
+        self.button_send = tk.Button(master=frame, text="depense", command=self.send)
         self.button_send.pack()
         
-        self.button_recive = tk.Button(self.master, text="revenue", command=self.recive)
+        self.button_recive = tk.Button(master=frame, text="revenue", command=self.recive)
         self.button_recive.pack()
 
     def logout(self):
