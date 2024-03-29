6931b3ceca91e09bd7225f5c127faedf3cb358b7
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-03-29 08:44:40 +0100

    add frame and change position display for widget in RegisterScreen.py

--- src/view/RegisterScreen.py
+++ src/view/RegisterScreen.py
@@ -1,45 +1,46 @@
 import tkinter as tk
 from tkinter import messagebox
 
-class RegisterScreen:
+class RegisterScreen(tk.Frame):
     def __init__(self, master):
-        self.master = master
-        self.master.title("Register")
-        self.master.geometry("300x250")
+        super().__init__(master)
         
-        self.status = None
+        self.statut = None
         self.information = []
         self.widgets()
 
     def widgets(self):
-        self.label_first_name = tk.Label(self.master, text="Nom")
+        frame = tk.Frame(master=self)
+        frame.pack()
+        
+        self.label_first_name = tk.Label(master=frame, text="Nom")
         self.label_first_name.pack()
         
-        self.entry_first_name = tk.Entry(self.master)
+        self.entry_first_name = tk.Entry(master=frame)
         self.entry_first_name.pack()
         
-        self.label_last_name = tk.Label(self.master, text="Prénom")
+        self.label_last_name = tk.Label(master=frame, text="Prénom")
         self.label_last_name.pack()
         
-        self.entry_last_name = tk.Entry(self.master)
+        self.entry_last_name = tk.Entry(master=frame)
         self.entry_last_name.pack()
         
-        self.label_email = tk.Label(self.master, text="Email")
+        self.label_email = tk.Label(master=frame, text="Email")
         self.label_email.pack()
         
-        self.entry_email = tk.Entry(self.master)
+        self.entry_email = tk.Entry(master=frame)
         self.entry_email.pack()
         
-        self.label_password = tk.Label(self.master, text="Password")
+        self.label_password = tk.Label(master=frame, text="Password")
         self.label_password.pack()
         
-        self.entry_password = tk.Entry(self.master, show="*")
+        self.entry_password = tk.Entry(master=frame, show="*")
         self.entry_password.pack()
         
-        self.button_register = tk.Button(self.master, text="Register", command=self.register)
+        self.button_register = tk.Button(master=frame, text="Register", command=self.register)
         self.button_register.pack()
         
-        self.login = tk.Button(self.master, text="Login", command=self.login)
+        self.login = tk.Button(master=frame, text="Login", command=self.login)
         self.login.pack()
 
     def register(self):
@@ -48,10 +49,10 @@ class RegisterScreen:
         password = self.entry_password.get()
         
         if username != "" and email != "" and password != "":
-            self.status = "register"
+            self.statut = "register"
             self.information = [username, email, password]
         else:
             tk.messagebox.showerror("Erreur", "Veuillez remplir tous les champs")
 
     def login(self):
-        self.status = "login"
+        self.statut = "login"
