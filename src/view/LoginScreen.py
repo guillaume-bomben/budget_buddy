fcfb353825e4819f3b4000dc3440092fcd746dfe
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-04-01 21:55:45 +0200

    change display in Login Page

--- src/view/LoginScreen.py
+++ src/view/LoginScreen.py
@@ -19,17 +19,20 @@ class LoginScreen(tk.Frame):
         self.entry_email = tk.Entry(master=frame)
         self.entry_email.pack()
         
-        self.label_password = tk.Label(master=frame,text="password")
+        self.label_password = tk.Label(master=frame,text="mot de passe")
         self.label_password.pack()
         
         self.entry_password = tk.Entry(master=frame,show="*")
         self.entry_password.pack()
         
-        self.button_login = tk.Button(master=frame,text="Login",command=self.login)
-        self.button_login.pack()
+        self.frame_button = tk.Frame(master=frame)
+        self.frame_button.pack(pady=10)
         
-        self.register = tk.Button(master=frame,text="Register",command=self.register)
-        self.register.pack()
+        self.button_login = tk.Button(self.frame_button,text="Conexion",command=self.login)
+        self.button_login.grid(row=0,column=0 ,padx=5)
+        
+        self.button_register = tk.Button(self.frame_button,text="Inscription",command=self.register)
+        self.button_register.grid(row=0,column=1,padx=5)
     
     def login(self):
         email = self.entry_email.get()
@@ -42,4 +45,4 @@ class LoginScreen(tk.Frame):
             tk.messagebox.showerror("Erreur","Veuillez remplir tous les champs")
     
     def register(self):
-        self.statut = "register"
+        self.statut = "register page"
