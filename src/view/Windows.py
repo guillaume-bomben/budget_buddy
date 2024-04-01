25e0757423b92ee7c90f39fe56888fdef2a0a8a3
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-04-01 18:57:51 +0200

    add display for transaction screen

--- src/view/Windows.py
+++ src/view/Windows.py
@@ -1,10 +1,12 @@
 from src.view.LoginScreen import LoginScreen
 from src.view.RegisterScreen import RegisterScreen
+from src.view.transactionScreen import TransactionScreen
 from src.view.home import Home
 import tkinter as tk
 import threading
 import time
 import os
+
 
 class Windows:
     def __init__(self):
@@ -25,25 +27,23 @@ class Windows:
         self.log_screen = LoginScreen(self.master)
         self.log_screen.pack()
         print("login")
-        
+        self.thread_flag = True
         def check():
-            self.thread_flag = True
             while self.thread_flag:
                 if self.log_screen.statut == "login":
                     self.statut = "login"
+                    self.log_screen.statut = None
                     self.information = self.log_screen.information
                     print(self.information)
-                    break
+                    self.thread_flag = False
                 elif self.log_screen.statut == "register":
+                    self.log_screen.statut = None
                     self.statut = "register"
-                    break
+                    self.thread_flag = False
                 else:
                     print("waiting_1")
                 time.sleep(1)
         threading.Thread(target=check).start()
-
-    def forget_login(self):
-        self.log_screen.pack_forget()
 
     def home_page(self):
         self.home = Home(self.master)
@@ -53,14 +53,21 @@ class Windows:
             self.thread_flag = True
             while self.thread_flag:
                 if self.home.statut == "logout":
+                    self.home.statut = None
                     self.statut = "logout"
-                    break
+                    self.thread_flag = False
                 elif self.home.statut == "send":
+                    self.home.statut = None
                     self.statut = "send"
-                    break
+                    self.thread_flag = False
                 elif self.home.statut == "recive":
+                    self.home.statut = None
                     self.statut = "recive"
-                    break
+                    self.thread_flag = False
+                elif self.home.statut == "transaction page":
+                    self.home.statut = None
+                    self.statut = "transaction page"
+                    self.thread_flag = False
                 else:
                     print("waiting_2")
                 time.sleep(1)
@@ -74,12 +81,32 @@ class Windows:
         def check():
             self.thread_flag = True
             while self.thread_flag:
-                if self.register_screen.statut == "register":
-                    self.statut = "register"
+                if self.register_screen.statut == "Create account":
+                    self.register_screen.statut = None
+                    self.statut = "Create account"
                     self.information = self.register_screen.information
                     print(self.information)
-                    break
+                    self.thread_flag = False
                 else:
                     print("waiting_3")
+                time.sleep(1)
+        threading.Thread(target=check).start()
+    
+    
+    def transaction_screen(self,user_list):
+        self.transaction_screen = TransactionScreen(self.master,user_list)
+        self.transaction_screen.pack()
+        
+        def check():
+            self.thread_flag = True
+            while self.thread_flag:
+                if self.transaction_screen.statut == "transaction":
+                    self.transaction_screen.statut = None
+                    self.statut = "transaction"
+                    self.information = self.transaction_screen.information
+                    print(self.information)
+                    self.thread_flag = False
+                else:
+                    print("waiting_4")
                 time.sleep(1)
         threading.Thread(target=check).start()
