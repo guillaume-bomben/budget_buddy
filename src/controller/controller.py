eca5d110a22764dfeb851cac5f6e591dab4b76d4
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-04-01 20:44:47 +0200

    add changing logic and display for home page to transaction list page

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -40,6 +40,18 @@ class controller:
     def home_to_transaction(self):
         self.windows.home.pack_forget()
         self.windows.transaction_screen(self.user.user_list)
+    
+    def home_to_operation_list(self):
+        self.windows.home.pack_forget()
+        transaction_list = self.transaction.get_transaction_list(self.id_user)
+        new_transaction_list = []
+        print(transaction_list)
+        for i in range(len(transaction_list)):
+            print(self.user.get_user(transaction_list[i][1])[0][1])
+            user_send = self.user.get_user(transaction_list[i][0])[0][1]
+            user_recive = self.user.get_user(transaction_list[i][1])[0][1]
+            new_transaction_list.append([user_send,user_recive,transaction_list[i][2],transaction_list[i][3],transaction_list[i][4]])
+        self.windows.list_transaction_screen(new_transaction_list)
 
     def register(self,lastname,firstname,email,password):
         if self.user.verify_email(email) != []:
@@ -80,3 +92,6 @@ class controller:
                         self.user.update_balance(self.id_user,self.user.get_balance(self.id_user)+self.windows.information[1])
                     else:
                         self.user.update_balance(self.id_user,self.user.get_balance(self.id_user)-self.windows.information[1])
+            elif self.windows.statut == "transaction list":
+                self.windows.statut = None
+                self.home_to_operation_list()
